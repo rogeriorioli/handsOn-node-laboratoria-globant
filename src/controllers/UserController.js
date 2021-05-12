de3a4ed7f30fdf9b1cd3c125c20a41c7fc86abd0
Rogerio Orioli <rogerio@designhope.com.br>
2021-05-12 20:29:38 -0300

    sencond day  put and delete mode

--- src/controllers/UserController.js
+++ src/controllers/UserController.js
@@ -1,20 +1,49 @@
+const uuid = require('uuid')
 
 
-const user = [];
 
+//CRUD 
+
+// c = create
+// r = read 
+// u = update 
+// d = delete 
+
+
+
+
+const user = []; //fake database
 
 class UserController {
 
 
+
+  //read
   async index(req, res) {
     res.json(user)
   }
 
+  //read 
+  async getUser(req, res) {
+
+    const { id } = req.params
+
+    const userUnique = await user.find(user => user.id === id)
+
+    res.json({
+      userUnique
+    })
+
+  }
+
+
+  //create
   async createUser(req, res) {
 
     const { email, password } = req.body
 
     user.push({
+      id: uuid.v4(), //auto increment
       email,
       password
     })
@@ -22,6 +51,35 @@ class UserController {
     res.json(user)
 
   }
+
+
+  async updateUser(req, res) {
+
+    const { id } = req.params
+    const { email, password } = req.body
+
+    const userUnique = await user.find(user => user.id === id)
+
+    userUnique.email = email
+
+    res.json({
+      userUnique
+    })
+
+  }
+
+  async deleteUser(req, res) {
+
+    const { id } = req.params
+
+    const userId = await user.find(user => user.id === id)
+
+    const removedUser = user.filter(user => user.id !== userId.id)
+
+    res.json(removedUser)
+
+  }
+
 
 }
 
